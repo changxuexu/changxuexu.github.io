bda8be78edc1f7e517046ae07349b535a0a600c3
changxue <1538647982@qq.com>
2022-09-26 09:18:11 +0800

    1

--- nav/static/js/data.js
+++ nav/static/js/data.js
@@ -213,6 +213,7 @@ var data_jquery = {
       second_tit: '文档资料',
       second_child: [
         { name: 'jquery文档', href: 'http://jquery.cuishifeng.cn', title: '' },
+        { name: 'jquery官网', href: 'https://jquery.com', title: '' },
         { name: 'zepto文档', href: 'https://www.html.cn/doc/zeptojs_api', title: '' }
       ]
     },
