927f3c24797e02472561c309d368abf98c6c5cfe
changxue <1538647982@qq.com>
2024-01-04 16:19:49 +0800

    1

--- 案例/router-advanced/static/sc-router.js
+++ 案例/router-advanced/static/sc-router.js
@@ -1,30 +1,51 @@
-
 'use strict';
 
 class SCRouter extends HTMLElement {
+  constructor() {
+    super();
+
+    this._onChanged = this._onChanged.bind(this);
+    this._routes = new Map();
+    /* 
+    // 在这里添加自定义元素的 DOM 结构和逻辑
+    this.attachShadow({ mode: 'open' }); 
+    // 创建 Shadow DOM 树
+    const shadowRoot = this.shadowRoot;
+    shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          padding: 8px;
+          background-color: lightblue;
+        }
+      </style>
+      <h2>Hello, I'm a custom element!</h2>
+      <p>My content goes here...</p>
+    `; 
+    */
+  }
 
   _onChanged () {
     const path = window.location.pathname;
     const routes = Array.from(this._routes.keys());
-    const route = routes.find(r => r.test(path));
+    const route = routes.find(r => r.test(path)); //当前页面正则
     const data = route.exec(path);
 
     if (!route) {
       return;
     }
 
-    // Store the new view.
+    // 存储当前页面 view
     this._newView = this._routes.get(route);
 
-    // We don't want to create more promises for the outgoing view animation,
-    // because then we get a lot of hanging Promises, so we add a boolean gate
-    // here to stop if there's already a transition running.
+    // 我们不想在离场视图动画中创建更多的Promise对象，因为这样一来会导致很多Promise挂起无法及时解决。
+    // 因此，我们在這裡添加了一个布尔标志，以便在已有过渡动画正在进行时停止创建新的Promise。
     if (this._isTransitioningBetweenViews) {
       return Promise.resolve();
     }
     this._isTransitioningBetweenViews = true;
 
-    // Assume that there's no outgoing animation required.
+    // 假设不需要进行离场动画。
     let outViewPromise = Promise.resolve();
 
     // If there is a current view...
@@ -58,10 +79,10 @@ class SCRouter extends HTMLElement {
     return this._onChanged();
   }
 
+  // route: "^/misc/(.*)"
   addRoute (route, view) {
     if (this._routes.has(route))
       return console.warn(`Route already exists: ${route}`);
-
     this._routes.set(route, view);
   }
 
@@ -70,7 +91,6 @@ class SCRouter extends HTMLElement {
     views.forEach(view => {
       if (!view.route)
         return;
-
       this.addRoute(new RegExp(view.route, 'i'), view);
     }, this);
   }
@@ -83,22 +103,21 @@ class SCRouter extends HTMLElement {
     this._routes.clear();
   }
 
-  createdCallback () {
-    this._onChanged = this._onChanged.bind(this);
-    this._routes = new Map();
-  }
-
-  attachedCallback () {
+  // 生命周期钩子
+  // 早期：createdCallback attachedCallback detachedCallback
+  // 目前：constructor、connectedCallback、disconnectedCallback
+  // DOM挂载完成
+  connectedCallback () {
     window.addEventListener('popstate', this._onChanged);
     this._clearRoutes();
     this._addRoutes();
     this._onChanged();
   }
 
-  detachedCallback () {
+  // DOM移除完成
+  disconnectedCallback () {
     window.removeEventListener('popstate', this._onChanged);
   }
 }
 
-// document.registerElement('sc-router', SCRouter);
 window.customElements.define('sc-router', SCRouter)
