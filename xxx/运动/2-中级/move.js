e65e9cbad856d2f3a0e0b4656088bb9631d98d8b
changxue <1538647982@qq.com>
2024-01-31 17:34:15 +0800

    1

--- xxx/运动/2-中级/move.js
+++ xxx/运动/2-中级/move.js
@@ -1,46 +1,28 @@
-function getStyle(obj, attr)
-{
-	if(obj.currentStyle)
-	{
+function getStyle(obj, attr){
+	if(obj.currentStyle){
 		return obj.currentStyle[attr];
 	}
-	else
-	{
-		return getComputedStyle(obj, false)[attr];
-	}
+	return getComputedStyle(obj, false)[attr];
 }
 
-function startMove(obj, attr, iTarget)
-{
-	clearInterval(obj.timer);
+function startMove(obj, attr, iTarget){
+	obj.timer && clearInterval(obj.timer);
 	obj.timer=setInterval(function (){
 		var iCur=0;
-		
-		if(attr=='opacity')
-		{
+		if(attr=='opacity'){
 			iCur=parseInt(parseFloat(getStyle(obj, attr))*100);
-		}
-		else
-		{
+		}else{
 			iCur=parseInt(getStyle(obj, attr));
 		}
-		
 		var iSpeed=(iTarget-iCur)/8;
 		iSpeed=iSpeed>0?Math.ceil(iSpeed):Math.floor(iSpeed);
-		
-		if(iCur==iTarget)
-		{
+		if(iCur==iTarget){
 			clearInterval(obj.timer);
-		}
-		else
-		{
-			if(attr=='opacity')
-			{
+		}else{
+			if(attr=='opacity'){
 				obj.style.filter='alpha(opacity:'+(iCur+iSpeed)+')';
 				obj.style.opacity=(iCur+iSpeed)/100;
-			}
-			else
-			{
+			}else{
 				obj.style[attr]=iCur+iSpeed+'px';
 			}
 		}
