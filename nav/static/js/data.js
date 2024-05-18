9e28b64fb5a295749037c7956ee5c8a0103d0021
changxue <1538647982@qq.com>
2024-05-18 09:24:35 +0800

    1

--- nav/static/js/data.js
+++ nav/static/js/data.js
@@ -125,8 +125,8 @@ var data_vue = {
     {
       second_tit: '组件库M',
       second_child: [
-        { name: 'Vant2', href: 'https://vant-contrib.gitee.io/vant/v2/#/zh-CN', title: '有赞' },
-        { name: 'Vant3', href: 'https://vant-contrib.gitee.io/vant/v3/#/zh-CN', title: '有赞' },
+        { name: 'Vant2', href: 'https://vant-ui.github.io/vant/v2/#/zh-CN/', title: '有赞' },
+        { name: 'Vant3', href: 'https://vant-ui.github.io/', title: '有赞' },
         { name: 'NutUI', href: 'https://nutui.jd.com/#/intro', title: '京东' },
         { name: 'Mint', href: 'http://mint-ui.github.io/#!/zh-cn', title: '饿了么' },
         { name: 'Cube UI', href: 'https://didi.github.io/cube-ui/#/zh-CN', title: '滴滴' },
@@ -896,7 +896,8 @@ var data_jsplug = {
         { name: 'Vue Phone Number', href: 'https://github.com/LouisMazel/vue-phone-number-input', title: '电话号码：有效的国家/地区代码、主题和颜色、电话号码格式' },
         { name: '二维码vue', href: 'https://link-to-qr.com', title: '' },
         { name: '跨路由动画_vue-starport', href: 'https://github.com/antfu/vue-starport', title: '仅支持Vue3，实现跨路由的共享组件动画效果' },
-        { name: '打印/转pdf_vue-plugin-hiprint', href: 'https://gitee.com/CcSimple/vue-plugin-hiprint', title: '基于hiprint：http://hiprint.io/；JS打印插件，快速分页预览、打印、转pdf' }
+        { name: '打印/转pdf_vue-plugin-hiprint', href: 'https://gitee.com/CcSimple/vue-plugin-hiprint', title: '基于hiprint：http://hiprint.io/；JS打印插件，快速分页预览、打印、转pdf' },
+        { name: 'Demo_vue-plugin-hiprint', href: 'https://github.com/CcSimple/vue-plugin-hiprint-start', title: '入门案例，教程：https://segmentfault.com/a/1190000043633722' }
       ]
     },
     {
